5312eb57c0ebdd74ca4c79d71fbc27a3abde8ba1
Andrey <a>
2019-03-31 16:17:04 +0300

    commit doc

--- src/main/java/com/manyatkin/springioex/AccessingRelationalDatausingJDBCwithSpring/AccessingRelationalDataUsingJdbcWithSpringApplication.java
+++ src/main/java/com/manyatkin/springioex/AccessingRelationalDatausingJDBCwithSpring/AccessingRelationalDataUsingJdbcWithSpringApplication.java
@@ -13,6 +13,38 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+//Spring Boot supports H2, an in-memory relational database engine,
+// and automatically creates a connection.
+// Because we are using spring-jdbc, Spring Boot automatically creates a JdbcTemplate.
+// The @Autowired JdbcTemplate field automatically loads it and makes it available.
+//
+// This Application class implements Spring Boot’s CommandLineRunner,
+// which means it will execute the run() method after the application context is loaded up.
+//
+//    First, you install some DDL using JdbcTemplate’s `execute method.
+//
+//Second, you take a list of strings and using Java 8 streams,
+// split them into firstname/lastname pairs in a Java array.
+//
+//Then you install some records in your newly created table using JdbcTemplate’s `batchUpdate method.
+// The first argument to the method call is the query string,
+// the last argument (the array of Object s) holds the variables to be substituted into the query where the “?” characters are.
+
+//For single insert statements, JdbcTemplate’s `insert method is good.
+// But for multiple inserts, it’s better to use batchUpdate.
+
+//Use ? for arguments to avoid SQL injection attacks by instructing JDBC to bind variables.
+
+
+//Finally you use the query method to search your table for records matching the criteria.
+// You again use the “?” arguments to create parameters for the query, passing in the actual values when you make the call.
+// The last argument is a Java 8 lambda used to convert each result row into a new Customer object.
+//
+//Java 8 lambdas map nicely onto single method interfaces, like Spring’s RowMapper.
+// If you are using Java 7 or earlier, you can easily plug in an anonymous interface implementation
+// and have the same method body as the lambda expresion’s body contains, and it will work with no fuss from Spring.
+
+
 @SpringBootApplication
 public class AccessingRelationalDataUsingJdbcWithSpringApplication implements CommandLineRunner {
 
